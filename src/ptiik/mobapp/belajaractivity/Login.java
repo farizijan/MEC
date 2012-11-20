a036b3e39fbb6ec749545978c2d053f4b557c105
Fariz Izzan <farizijan@in-com.(none)>
2012-11-20 21:52:12 +0700

    Login sudah menggunakan webservice. uname dan password menggunakan nama depan masing-masing rek.

--- src/ptiik/mobapp/belajaractivity/Login.java
+++ src/ptiik/mobapp/belajaractivity/Login.java
@@ -4,17 +4,34 @@
  */
 package ptiik.mobapp.belajaractivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ptiik.mobapp.belajaractivity.R;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.text.InputFilter.LengthFilter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends Activity implements OnClickListener {
 
+	private static final String url_login = "http://farizijan.com/mobapp/get_user.php";
+	JSONParser jsonParser = new JSONParser();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +47,7 @@ public class Login extends Activity implements OnClickListener {
         return true;
     }
     public static String username;
+    public static String password;
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
@@ -39,17 +57,89 @@ public class Login extends Activity implements OnClickListener {
 		String vpass=pass.getText().toString();
 		switch (v.getId()) {
 		case R.id.button1:
-			if("b".equals(vpass)){
-				finish();
-				Intent i = new Intent(this, MainMenu.class );
-				startActivity(i);
+			//if("b".equals(vpass)){
+				//finish();
+				//Intent i = new Intent(this, MainMenu.class );
+				//startActivity(i);
 				username=vUname;
-			}
-			else {
-				Intent i = new Intent(this, LoginAlert.class );
-				startActivity(i);
-			}
-			break;
+				password=vpass;
+				new LoginProcess().execute();
+			//}
+			//else {
+				//Intent i = new Intent(this, LoginAlert.class );
+				//startActivity(i);
+			//}
+			//break;
     	}
+	}
+	
+	class LoginProcess extends AsyncTask<String, String, String> {
+
+		ProgressDialog pDialog;
+		int success;
+		/**
+		 * Before starting background thread Show Progress Dialog
+		 * */
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			 pDialog = new ProgressDialog(Login.this);
+			pDialog.setMessage("Login...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+		}
+
+		/**
+		 * Getting product details in background thread
+		 * */
+		protected String doInBackground(String... params) {
+
+					
+					Log.d("Login","username:"+username+", password: "+password);
+					// Building Parameters
+					List<NameValuePair> params2 = new ArrayList<NameValuePair>();
+					params2.add(new BasicNameValuePair("username", username));
+					params2.add(new BasicNameValuePair("password", password));
+
+					// getting product details by making HTTP request
+					// Note that product details url will use GET request
+					JSONObject json = jsonParser.makeHttpRequest(
+							url_login, "GET", params2);
+
+					// check your log for json response
+					Log.d("Login Details", json.toString());
+					
+					try {
+						
+						
+						// json success tag
+						success = json.getInt("success");
+						
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				//}
+			//});
+
+			return null;
+		}
+
+
+		/**
+		 * After completing background task Dismiss the progress dialog
+		 * **/
+		protected void onPostExecute(String file_url) {
+			
+			// dismiss the dialog once got all details
+			pDialog.dismiss();
+			if (success == 1) {
+				Intent i = new Intent(getApplicationContext(), MainMenu.class);
+				startActivity(i);
+				finish();
+			}else{
+				Toast.makeText(getApplicationContext(), "Username atau password salah!", Toast.LENGTH_LONG).show();
+			}
+		}
 	}
 }
